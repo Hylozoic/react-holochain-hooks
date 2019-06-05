3d23c88964b856aace9ac467781236c12a77bf12
Robbie Carlton <robbie.carlton@gmail.com>
2019-06-05 10:07:01 -0600

    fix test!

--- __mocks__/@holochain/hc-web-client.js
+++ __mocks__/@holochain/hc-web-client.js
@@ -1,16 +1,9 @@
-function connect (url) {
-  console.log('calling from inside the mocked connect, url', url)    
-  const callZome = () => console.log('*** calling mocked callZomeRef')
+export const callZome = jest.fn()
+export const call = jest.fn()
+export const close = jest.fn()
 
-  const call = () => console.log('*** calling mocked callRef')
-
-  const close = () => console.log('*** calling mocked closeRef')
-
+export function connect () {
   return Promise.resolve({
     callZome, call, close
   })
-}
-
-module.exports = {
-  connect
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,31 +1,30 @@
 import 'react'
 import useHolochainWebClient from './index'
 import React from 'react'
-require('@holochain/hc-web-client')
-import { render } from 'react-testing-library'
-
+import { render } from '@testing-library/react'
+import { callZome, call, close } from '../__mocks__/@holochain/hc-web-client'
 
 describe('useHolochainWebClient', () => {
   const TestComponent = () => {  
-    console.log('1')
-    useHolochainWebClient(' once ')
-    console.log('2')
-    const { callZomeRef, callRef, closeRef } = useHolochainWebClient(' twice ')
-
-    console.log('3')
-    console.log('callZomeRef', callZomeRef.current)
-    console.log('callRef', callRef.current)  
-    console.log('closeRef', closeRef.current)    
+    const { callZomeRef, callRef, closeRef } = useHolochainWebClient()
   
-    console.log('callZomeRef()', callZomeRef.current())
-    console.log('callRef()', callRef.current())  
-    console.log('closeRef()', closeRef.current())      
-    return <div />
+    callZomeRef.current('callZomeArg')
+    callRef.current('callArg')
+    closeRef.current('closeArg')
+
+    return null
   }
 
   it('imports the file', () => {  
-    render(<TestComponent />)
-    expect('got there').toMatchSnapshot()
+    callZome.mockClear()
+    call.mockClear()
+    close.mockClear()
+    render(<TestComponent />)    
+    setTimeout(() => {
+      expect(callZome).toHaveBeenCalledWith('callZomeArg')
+      expect(call).toHaveBeenCalledWith('callArg')    
+      expect(close).toHaveBeenCalledWith('closeArg')      
+    }, 0)
   })
 })
 
